7594a898d531698f8bf10e21799b8e9e281c010a
Nathan Struhs <nathanstruhs@gmail.com>
2017-09-23 12:51:50 -0700

    First iteration of Bob exercise

---
+++ cpp/bob/bob.cpp
@@ -0,0 +1,39 @@
+#include "bob.h"
+
+bool is_question(std::string remark) {
+	return (remark[remark.length() -1] == '?');
+}
+
+bool is_yell(std::string remark) {
+	bool flag = false;
+	for (int i = 0; i < remark.length(); i++) {
+		if (isalpha(remark[i])) {
+			if (isupper(remark[i])) {
+				flag = true;
+			} else {
+				return false;
+			}
+		}
+	}
+	return flag;
+}
+
+std::string remove_whitespace(std::string remark) {
+	remark.erase(remove_if(remark.begin(), remark.end(), isspace), remark.end());
+	return remark;
+}
+
+std::string bob::hey(std::string remark) {
+	remark = remove_whitespace(remark);
+	std::string response = "Whatever.";
+
+	if (remark.empty()) {
+		response = "Fine. Be that way!";
+	} else if (is_yell(remark)) {
+		response = "Whoa, chill out!";
+	} else if (is_question(remark)) {
+		response = "Sure.";
+	}
+
+	return response;
+}
---
+++ cpp/bob/bob.h
@@ -0,0 +1,11 @@
+#if !defined(BOB_H)
+#define BOB_H
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+namespace bob {
+	std::string hey(std::string remark);
+}
+
+#endif
